a85ea94eab523b9c95fa4240f8db91d88d7581ef
IslanP1 <islanpereiradeoliveira@gmail.com>
2023-09-23 06:56:00 -0300

    feat: priorizando os jogos favoritos no topo

--- scripts/script.js
+++ scripts/script.js
@@ -15,10 +15,12 @@ function coletarDados() {
     arrayObjetos.push(objeto);
     console.log(arrayObjetos);
     renderizarJogos();
-};
+}
 
 function renderizarJogos() {
     let elementosHTML = "";
+    let jogosFavoritos = [];
+    let jogosNaoFavoritos = [];
     
     arrayObjetos.forEach((valor, index) => {
         const elemento = `
@@ -33,24 +35,26 @@ function renderizarJogos() {
                 <div class="direita">
                     <a onclick="removerJogos(${index})"><img class="img2" src="img/trash 1.svg" alt="lixinho"></a>
                     ${valor.favorito === "on" ?
-                    `<img class="img3" src="img/star-outline-svgrepo-com 1.svg" alt="estrela vazia">` :
+                    `<img class="img3" src="img/star-outline-svgrepo-com 1.svg" alt="estrela vazia">`:
                     `<img class="img3" src="img/star-offtline-svgrepo-com 1.svg" alt="estrela vazia">`}
                 </div>
             </div>
         `;
+        valor.favorito === "on" ? jogosFavoritos.push(elemento) : jogosNaoFavoritos.push(elemento);
         elementosHTML += elemento;
-    });
+        
+    })
 
-    listagem.innerHTML = elementosHTML;
-};
+    let ordenado = jogosFavoritos.concat(jogosNaoFavoritos).join('');
+    listagem.innerHTML = ordenado;
+}
 
 function removerJogos(index) {
     arrayObjetos.splice(index, 1);
     renderizarJogos();
-};
+}
 
 function favoritarJogos() {
     let valueCheckButton = document.querySelector("input[type=radio][name=devweb]:checked");
     return valueCheckButton.value;
-};
-
+}
